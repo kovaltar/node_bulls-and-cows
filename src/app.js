8037758301d37e5953dfb2c5a63fb70f0050b673
kovaltar <kovaltar@gmail.com>
2025-04-09 23:30:23 +0200

    The "Bulls & Cows" game solution

--- src/app.js
+++ src/app.js
@@ -1,3 +1,19 @@
 'use strict';
+import readline from 'node:readline';
+import { generateRandomNumber } from './modules/generateRandomNumber';
+import { getBullsAndCows } from './modules/getBullsAndCows';
+import { checkIsValidUserInput } from './modules/checkIsValidUserInput';
 
-// Write your code here
+const randNum = generateRandomNumber();
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question('Enter a number? ', (inputNum) => {
+  if (checkIsValidUserInput(inputNum)) {
+    getBullsAndCows(inputNum, randNum);
+  }
+
+  rl.close();
+});
